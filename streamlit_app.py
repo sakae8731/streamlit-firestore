2987da96e353263e49039b3b6f498cf568164008
Emi <sakae8731@gmail.com>
2023-11-26 13:24:32 +0900

    iris

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
+from google.cloud import firestore
+db = firestore.Client.from_service_account_json("serviceAccountKey.json")
+docs = db.collection("iris").stream()
+data = []
+for d in docs:
+    data.append(d.to_dict())
 
-st.header("hello 🌎!")
-if st.button("Balloons?"):
-    st.balloons()
+species = st.sidebar.selectbox("species", ["setosa", "versicolor", "virginica"])
+
+df = pd.DataFrame(data)
+df = df[df["species"] == species].reset_index(drop=True)
+st.write(df)
+st.line_chart(df, x="sepal_length", y="sepal_width")
