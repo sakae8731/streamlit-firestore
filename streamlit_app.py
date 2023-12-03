ce87d3d1ad5c61d3eaba40668411e2fda18e1bde
Emi <sakae8731@gmail.com>
2023-12-03 13:21:26 +0900

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,23 @@ import streamlit as st
 import pandas as pd
 from google.cloud import firestore
 
-from google.oauth2 import service_account
-import json
-key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="streamlit-app-c394d")
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
 
-docs = db.collection("iris").stream()
-data = []
-for d in docs:
-    data.append(d.to_dict())
+# from google.oauth2 import service_account
+# import json
+# key_dict = json.loads(st.secrets["textkey"])
+# creds = service_account.Credentials.from_service_account_info(key_dict)
+# db = firestore.Client(credentials=creds, project="streamlit-app-c394d")
 
-species = st.sidebar.selectbox("species", ["setosa", "versicolor", "virginica"])
+# docs = db.collection("iris").stream()
+# data = []
+# for d in docs:
+#     data.append(d.to_dict())
 
-df = pd.DataFrame(data)
-df = df[df["species"] == species].reset_index(drop=True)
-st.write(df)
-st.line_chart(df, x="sepal_length", y="sepal_width")
+# species = st.sidebar.selectbox("species", ["setosa", "versicolor", "virginica"])
+
+# df = pd.DataFrame(data)
+# df = df[df["species"] == species].reset_index(drop=True)
+# st.write(df)
+# st.line_chart(df, x="sepal_length", y="sepal_width")
